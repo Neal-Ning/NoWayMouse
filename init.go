93d4c0eba681ad03a55ce17e710128538c2a2f37
Neal Ning <yichen.ning.2021@gmail.com>
2025-07-17 19:13:49 +0200

    Major overlay / division mode overhaul, user now has more freedome in defining config. Implemented the mouse moving part of the overhaul.

---
+++ init.go
@@ -0,0 +1,306 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"unicode/utf8"
+
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+
+	"github.com/bendahl/uinput"
+	"github.com/gvalkov/golang-evdev"
+	"gopkg.in/yaml.v3"
+)
+
+// =============================== //
+// ========== Variables ========== //
+// =============================== //
+
+var (
+	// Config loader
+	config Config
+
+	// ========== Path variables ========== //
+
+	socketPath string = "/tmp/overlay.sock"
+	defaultConfigPath string = getScriptPath("default.yaml")
+	userConfigPath string = filepath.Join("/home", userName(), ".config", "nowaymouse", "config.yaml")
+
+	// ========== Configuratble variables ========== //
+
+	// The keyboard input device: /dev/input/eventn
+	keyboardPath string
+
+	// Mouse mode keybinds
+	activationKey string
+	divKey string
+	mouseClick string
+	mouseUp string
+	mouseLeft string
+	mouseDown string
+	mouseRight string
+	scrollUp string
+	scrollLeft string
+	scrollDown string
+	scrollRight string
+	mouseSpeed int32
+	scrollSpeed int32
+
+	// Resolution
+	resX int32
+	resY int32
+
+	// Number of divisions
+	nDivs int
+
+	// Dimension of the divisions
+	divDim [][]int32
+
+	// Keys used to navigate the divisions
+	divKeys [][]string
+
+	// ========== Inferrable variables ========== //
+
+	// Mappings from defined keys to their index
+	divKeyMaps []map[string]int32
+	
+	// Dimensions of each box in the divisions
+	divArea [][2]int32
+
+	// Longest navigator keys for a division
+	longestKeyLen []int
+
+	// ========== Software and application variables ========== //
+
+	// Simulation / Program variables
+	keyInput *evdev.InputDevice // Registered keyboard
+	keyboard uinput.Keyboard // Virtual keyboard
+	mouse uinput.Mouse // Virtual mouse
+	overlayProc *exec.Cmd // Overlay python process
+
+	// System environment variables
+	waybarHeight int32 = 30 // Height of the waybar
+	display string = os.Getenv("DISPLAY")
+	wayland string = os.Getenv("WAYLAND_DISPLAY")
+	runtimeDir string = os.Getenv("XDG_RUNTIME_DIR")
+	dbus string = ("DBUS_SESSION_BUS_ADDRESS")
+
+	// State variables
+	adjustMode int = 2
+	mouseMode bool = false // Keyboard controls mouse
+	divMode bool = false // Overlay to divide screen into boxes
+	divCount int = -1 // Number of divisions currently done
+	currentDivBoxX int32 = 0 // X of corner of the box selected from the last division
+	currentDivBoxY int32 = 0 // Y of corner of the box selected from the last division
+	pressed string = ""// String of pressed (navigation) keys during the current division
+
+	// Other
+	heldMovementKeys map[string]bool
+	movementMutex = sync.RWMutex{}
+)
+
+// ============================ //
+// ========== Config ========== //
+// ============================ //
+
+
+// Config variable mapping
+type Config struct {
+	KeyboardPath string `yaml:"keyboard_input_path"`
+	ActivationKey string `yaml:"activation_key"`
+	DivKey string `yaml:"activate_division_overlay_key"`
+	MouseClick string `yaml:"mouse_click"`
+	MouseUKey string `yaml:"mouse_up"`
+	MouseLKey string `yaml:"mouse_left"`
+	MouseDKey string `yaml:"mouse_down"`
+	MouseRKey string `yaml:"mouse_right"`
+	ScrollUKey string `yaml:"scroll_up"`
+	ScrollLKey string `yaml:"scroll_left"`
+	ScrollDKey string `yaml:"scroll_down"`
+	ScrollRKey  string `yaml:"scroll_right"`
+	MouseSpeed int32 `yaml:"mouse_speed"`
+	ScrollSpeed int32 `yaml:"scroll_speed"`
+	ResX int32 `yaml:"screen_x_resolution"`
+	ResY int32 `yaml:"screen_y_resolution"`
+	NDivs int `yaml:"number_of_divisions"`
+	DivDim [][]int32 `yaml:"division_dimensions"`
+	DivKeys	[][]string `yaml:"division_navigators"`
+}
+
+// Function that loads the config
+func load_config(path string) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println("Read config error: ", err)
+	}
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		fmt.Println("Parse config error: ", err)
+	}
+}
+
+// Define variables after loading default and user config
+func set_config() {
+	keyboardPath = config.KeyboardPath
+	activationKey = config.ActivationKey
+	divKey = config.DivKey
+	mouseClick = config.MouseClick
+	mouseUp = config.MouseUKey
+	mouseLeft = config.MouseLKey
+	mouseDown = config.MouseDKey
+	mouseRight = config.MouseRKey
+	scrollUp = config.ScrollUKey
+	scrollLeft = config.ScrollLKey
+	scrollDown = config.ScrollDKey
+	scrollRight = config.ScrollRKey
+	mouseSpeed = config.MouseSpeed
+	scrollSpeed = config.ScrollSpeed
+	resX = config.ResX
+	resY = config.ResY
+	nDivs = config.NDivs
+	divDim = config.DivDim
+	divKeys = config.DivKeys
+}
+
+// Verify that the defined config is sound
+func verify_config() {
+	if (nDivs != len(divDim)) {
+		panic(fmt.Sprintln("Defined number_of_divisions doesn't match the number of divisions dimensions given in division_dimensions."))
+	}
+	if (nDivs != len(divKeys)) {
+		panic(fmt.Sprintln("Defined number_of_divisions doesn't match the number of divisions dimensions inferred from division_navigators."))
+	}
+
+	var boxSizeX float32 = float32(resX)
+	var boxSizeY float32 = float32(resY)
+	for i, dim := range divDim {
+		if (len(dim) != 2 || dim[0] < 1 || dim[1] < 1) {
+			panic(fmt.Sprintln("The dimension of each division should be in the format: [x, y], where x, y >= 1."))
+		}
+
+		if (len(divKeys[i]) != int(dim[0] * dim[1])) {
+			panic(fmt.Sprintln("For a division, each list of navigators much match its corresponding division dimension."))
+		}
+
+		boxSizeX /= float32(dim[0])
+		boxSizeY /= float32(dim[1])
+	}
+	if (boxSizeX < 5 || boxSizeY < 5) {
+		panic(fmt.Sprintln("You have divided too much. The last division should at least have 5x5 grids."))
+	}
+}
+
+// Finalize config by inferring some necessary variables
+func finalize_config() {
+
+	// Create mapping of keys to their index for search up
+	divKeyMaps = make([]map[string]int32, nDivs)
+	longestKeyLen = make([]int, nDivs)
+	for i, keys := range divKeys {
+		divKeyMaps[i] = make(map[string]int32)
+		for j, key := range keys {
+			divKeyMaps[i][key] = int32(j)
+			// Record the longest navigation key for each division
+			var keyRuneCount int = utf8.RuneCountInString(key)
+			if longestKeyLen[i] < keyRuneCount {
+				longestKeyLen[i] = keyRuneCount
+			}
+		}
+	}
+
+	// Divided area of each division
+	divArea = make([][2]int32, nDivs+1)
+	divArea[0] = [2]int32{resX, resY}
+	for i, dim := range divDim {
+		divArea[i + 1][0] = divArea[i][0] / dim[0]
+		divArea[i + 1][1] = divArea[i][1] / dim[1]
+	}
+
+	// Define mapping of movement keys to their held state
+	heldMovementKeys = map[string]bool{
+		mouseUp: false,
+		mouseLeft: false,
+		mouseDown: false,
+		mouseRight: false,
+		scrollUp: false,
+		scrollLeft: false,
+		scrollDown: false,
+		scrollRight: false,
+	}
+}
+
+// Get name of current user
+func userName() string {
+	sudoUser := os.Getenv("SUDO_USER")
+	if (sudoUser != "") {
+		usr, err := user.Lookup(sudoUser)
+		if err == nil {
+			return usr.Username
+		}
+		fmt.Printf("Failed to lookup SUDO_USER (%s): %v \n", sudoUser, err)
+	}
+	return ""
+}
+
+// Get the folder contianing this script, and append python script name to it
+func getScriptPath(scriptName string) string {
+	exePath, err := os.Executable()
+	if err != nil {
+		fmt.Println("Failed to get executable path:", err)
+		os.Exit(1)
+	}
+	exeDir := filepath.Dir(exePath)
+	return filepath.Join(exeDir, scriptName)
+}
+
+// Open the keyboard located at keyboardPath to read inputs from it
+// Create a virtual keyboard so that some key presses can be filtered
+func initKeyboard() {
+	var err error
+	keyInput, err = evdev.Open(keyboardPath)
+	if err != nil {
+		fmt.Printf("Failed to open device: %v \n", err)
+	} else {
+		fmt.Printf("Opened keyboard device: %s\n", keyInput.Name)
+	}
+
+	keyboard, err = uinput.CreateKeyboard("/dev/uinput", []byte("virtual-keyboard"))
+	if err != nil {
+		fmt.Printf("Failed to create virtual keyboard: %v \n", err)
+	}
+}
+
+// Create a virtual mouse controller to move the mouse around
+func initMouse() {
+	var err error
+	mouse, err = uinput.CreateMouse("/dev/uinput", []byte("virtual-mouse"))
+	if err != nil {
+		fmt.Printf("Failed to create virtual mouse: %v \n", err)
+	}
+}
+
+// Start overlay python process with non-root environtment
+func initOverlay() {
+
+	pythonPath := getScriptPath(filepath.Join(".venv", "bin", "python"))
+    scriptPath := getScriptPath("overlay.py")
+
+    fullCmd := fmt.Sprintf("DISPLAY=%s WAYLAND_DISPLAY=%s XDG_RUNTIME_DIR=%s DBUS_SESSION_BUS_ADDRESS='%s' %s %s",
+        display, wayland, runtimeDir, dbus, pythonPath, scriptPath)
+
+	// Run command as non-root user with required environment variables
+    overlayProc := exec.Command("runuser", "-l", userName(), "-c", fullCmd)
+
+	// Display prints and errors from running the command
+	overlayProc.Stdout = os.Stdout
+	overlayProc.Stderr = os.Stderr
+
+	err := overlayProc.Start()
+	if err != nil {
+		fmt.Printf("Failed to start overlay: %v \n", err)
+		return
+	}
+}
--- main.go
+++ main.go
@@ -2,302 +2,13 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 
 	"net"
-	"os"
 	"os/exec"
-	"os/user"
-	"path/filepath"
 
-	"github.com/bendahl/uinput"
 	"github.com/gvalkov/golang-evdev"
-	"gopkg.in/yaml.v3"
 )
-
-var (
-	// Config
-	config Config
-
-	// ========== Path variables ========== //
-
-	socketPath string = "/tmp/overlay.sock"
-	defaultConfigPath string = getScriptPath("default.yaml")
-	userConfigPath string = filepath.Join("/home", userName(), ".config", "nowaymouse", "config.yaml")
-
-	// ========== Configuratble variables ========== //
-
-	// The keyboard input device: /dev/input/eventn
-	keyboardPath string
-
-	// Keybinds
-	activationKey string
-	overlayKey string
-	mouseClick string
-	mouseUp string
-	mouseLeft string
-	mouseDown string
-	mouseRight string
-	scrollUp string
-	scrollLeft string
-	scrollDown string
-	scrollRight string
-	mouseSpeed int32
-	scrollSpeed int32
-
-	// Resolution
-	resX int32
-	resY int32
-
-	// Number of cols and rows in the second division
-	div1Cols int32
-	div1Rows int32
-
-	// Keys used to navigate the divisions
-	div0XKeys []string
-	div0Y0Keys []string
-	div0Y1Keys []string
-	div1Keys []string
-
-	// ========== Inferrable variables ========== //
-
-	// Mappings from defined keys to their index
-	div0XKeyMap map[string]int32
-	div0Y0KeyMap map[string]int32
-	div0Y1KeyMap map[string]int32
-	div1KeyMap map[string]int32
-	
-	// Dimension of the first division
-	div0Cols int32
-	div0Rows int32
-
-	// Dimensions of each box in the first division
-	box0X int32
-	box0Y int32
-
-	// Dimensions of each box in the second division
-	box1X int32
-	box1Y int32
-
-	// ========== Software and application variables ========== //
-
-	// Simulation / Program variables
-	keyInput *evdev.InputDevice // Registered keyboard
-	keyboard uinput.Keyboard // Virtual keyboard
-	mouse uinput.Mouse // Virtual mouse
-	overlayProc *exec.Cmd // Overlay python process
-
-	// System environment variables
-	waybarHeight int32 = 30 // Height of the waybar
-	display string = os.Getenv("DISPLAY")
-	wayland string = os.Getenv("WAYLAND_DISPLAY")
-	runtimeDir string = os.Getenv("XDG_RUNTIME_DIR")
-	dbus string = ("DBUS_SESSION_BUS_ADDRESS")
-
-	// State variables
-	adjustMode int = 2
-	mouseMode bool = false // Keyboard controls mouse
-	overlayMode bool = false // Overlay to divide screen into boxes
-	overlay01 bool = false // Overlay showing the first divide, 1 input given
-	overlay02 bool = false // Overlay showing the first divide, 2 inputs given
-	overlay1 bool = false // Overlay showing the second divide, 1 input given
-
-	// Temporary state variables
-	selectedDiv0Col int32
-	selectedDiv0Row int32
-	heldMovementKeys map[string]bool
-	movementMutex = sync.RWMutex{}
-)
-
-// Config variable mapping
-type Config struct {
-	KeyboardPath string `yaml:"keyboard_input_path"`
-	ActivationKey string `yaml:"activation_key"`
-	OverlayKey string `yaml:"overlay_key"`
-	MouseClick string `yaml:"mouse_click"`
-	MouseUKey string `yaml:"mouse_up"`
-	MouseLKey string `yaml:"mouse_left"`
-	MouseDKey string `yaml:"mouse_down"`
-	MouseRKey string `yaml:"mouse_right"`
-	ScrollUKey string `yaml:"scroll_up"`
-	ScrollLKey string `yaml:"scroll_left"`
-	ScrollDKey string `yaml:"scroll_down"`
-	ScrollRKey  string `yaml:"scroll_right"`
-	MouseSpeed int32 `yaml:"mouse_speed"`
-	ScrollSpeed int32 `yaml:"scroll_speed"`
-	ResX int32 `yaml:"screen_x_resolution"`
-	ResY int32 `yaml:"screen_y_resolution"`
-	Div1Cols int32 `yaml:"div_1_n_cols"`
-	Div1Rows int32 `yaml:"div_1_n_rows"`
-	Div0XKeys []string `yaml:"div_0_first_key"`
-	Div0Y0Keys []string `yaml:"div_0_second_key_0"`
-	Div0Y1Keys []string `yaml:"div_0_second_key_1"`
-	Div1Keys []string `yaml:"div_1_key"`
-}
-
-// Get name of current user
-func userName() string {
-	sudoUser := os.Getenv("SUDO_USER")
-	if (sudoUser != "") {
-		usr, err := user.Lookup(sudoUser)
-		if err == nil {
-			return usr.Username
-		}
-		fmt.Printf("Failed to lookup SUDO_USER (%s): %v \n", sudoUser, err)
-	}
-	return ""
-}
-
-// Get the folder contianing this script, and append python script name to it
-func getScriptPath(scriptName string) string {
-	exePath, err := os.Executable()
-	if err != nil {
-		fmt.Println("Failed to get executable path:", err)
-		os.Exit(1)
-	}
-	exeDir := filepath.Dir(exePath)
-	return filepath.Join(exeDir, scriptName)
-}
-
-// Function that loads the config
-func load_config(path string) {
-	data, err := os.ReadFile(path)
-	if err != nil {
-		fmt.Println("Read config error: ", err)
-	}
-	err = yaml.Unmarshal(data, &config)
-	if err != nil {
-		fmt.Println("Parse config error: ", err)
-	}
-}
-
-// Define variables after loading default and user config
-func set_config() {
-	keyboardPath = config.KeyboardPath
-	activationKey = config.ActivationKey
-	overlayKey = config.OverlayKey
-	mouseClick = config.MouseClick
-	mouseUp = config.MouseUKey
-	mouseLeft = config.MouseLKey
-	mouseDown = config.MouseDKey
-	mouseRight = config.MouseRKey
-	scrollUp = config.ScrollUKey
-	scrollLeft = config.ScrollLKey
-	scrollDown = config.ScrollDKey
-	scrollRight = config.ScrollRKey
-	mouseSpeed = config.MouseSpeed
-	scrollSpeed = config.ScrollSpeed
-	resX = config.ResX
-	resY = config.ResY
-	div1Cols = config.Div1Cols
-	div1Rows = config.Div1Rows
-	div0XKeys = config.Div0XKeys
-	div0Y0Keys = config.Div0Y0Keys
-	div0Y1Keys = config.Div0Y1Keys
-	div1Keys = config.Div1Keys
-}
-
-// Finalize config by inferring some necessary variables
-func finalize_config() {
-
-	// Create mapping of keys to their index for search up
-	div0XKeyMap = make(map[string]int32)
-	for i, v := range div0XKeys {
-		div0XKeyMap[v] = int32(i)
-	}
-
-	div0Y0KeyMap = make(map[string]int32)
-	for i, v := range div0Y0Keys {
-		div0Y0KeyMap[v] = int32(i)
-	}
-
-	div0Y1KeyMap = make(map[string]int32)
-	for i, v := range div0Y1Keys {
-		div0Y1KeyMap[v] = int32(i)
-	}
-
-	div1KeyMap = make(map[string]int32)
-	for i, v := range div1Keys {
-		div1KeyMap[v] = int32(i)
-	}
-	
-	// Number of columns and rows in the first division
-	div0Cols = int32(len(div0XKeyMap))
-	div0Rows = int32(len(div0Y0KeyMap))
-
-	// Dimensions of each box in the first division
-	box0X = resX / div0Cols
-	box0Y = resY / div0Rows
-
-	// Dimensions of each box in the second division
-	box1X = box0X / div1Cols
-	box1Y = box0Y / div1Rows
-
-	heldMovementKeys = map[string]bool{
-		mouseUp: false,
-		mouseLeft: false,
-		mouseDown: false,
-		mouseRight: false,
-		scrollUp: false,
-		scrollLeft: false,
-		scrollDown: false,
-		scrollRight: false,
-	}
-}
-
-// Open the keyboard located at keyboardPath to read inputs from it
-// Create a virtual keyboard so that some key presses can be filtered
-func initKeyboard() {
-	var err error
-	keyInput, err = evdev.Open(keyboardPath)
-	if err != nil {
-		fmt.Printf("Failed to open device: %v \n", err)
-	} else {
-		fmt.Printf("Opened keyboard device: %s\n", keyInput.Name)
-	}
-
-	keyboard, err = uinput.CreateKeyboard("/dev/uinput", []byte("virtual-keyboard"))
-	if err != nil {
-		fmt.Printf("Failed to create virtual keyboard: %v \n", err)
-	} else {
-		fmt.Printf("Created virtual keyboard")
-	}
-}
-
-// Create a virtual mouse controller to move the mouse around
-func initMouse() {
-	var err error
-	mouse, err = uinput.CreateMouse("/dev/uinput", []byte("virtual-mouse"))
-	if err != nil {
-		fmt.Printf("Failed to create virtual mouse: %v \n", err)
-	} else {
-		fmt.Printf("Created virtual mouse")
-	}
-}
-
-// Start overlay python process with non-root environtment
-func initOverlay() {
-
-	pythonPath := getScriptPath(filepath.Join(".venv", "bin", "python"))
-    scriptPath := getScriptPath("overlay.py")
-
-    fullCmd := fmt.Sprintf("DISPLAY=%s WAYLAND_DISPLAY=%s XDG_RUNTIME_DIR=%s DBUS_SESSION_BUS_ADDRESS='%s' %s %s",
-        display, wayland, runtimeDir, dbus, pythonPath, scriptPath)
-
-	// Run command as non-root user with required environment variables
-    overlayProc := exec.Command("runuser", "-l", userName(), "-c", fullCmd)
-
-	// Display prints and errors from running the command
-	overlayProc.Stdout = os.Stdout
-	overlayProc.Stderr = os.Stderr
-
-	err := overlayProc.Start()
-	if err != nil {
-		fmt.Printf("Failed to start overlay: %v \n", err)
-		return
-	}
-}
 
 // Move the mouse to an absolute position
 func mouseAbs(x int32, y int32) {
@@ -306,55 +17,6 @@ func mouseAbs(x int32, y int32) {
 	// Move the mouse to defined position, with consideration to
 	// the fact that overlay.py does not cover the way bar
 	mouse.Move(x, y * (resY - waybarHeight) / resY + waybarHeight)
-}
-
-// Kill the overlay process
-func terminateOverLay() {
-	if overlayProc != nil && overlayProc.Process != nil {
-		_ = overlayProc.Process.Kill()
-	}
-	exec.Command("pkill", "-f", "overlay.py").Run()
-}
-
-// Display the overlay showing the first split
-func showOverlay0() {
-	messageOverlay([]byte("show0,100,100"))
-}
-
-// Display the overlay showing the second split
-// Parameters indicate which box was selected in the first split
-func showOverlay1(boxX int32, boxY int32) {
-	messageOverlay([]byte(fmt.Sprintf("show1,%v,%v", boxX, boxY)))
-}
-
-// Hide the overlay
-func hideOverlay() {
-	messageOverlay([]byte("hide,100,100"))
-}
-
-// Send message to overlay python process to change the overlay display
-func messageOverlay(msg []byte) {
-	conn, err := net.Dial("unix", socketPath)
-	if err != nil {
-		fmt.Println("Failed to connect to overlay socket: ", err)
-		return
-	}
-	defer conn.Close()
-	_, err = conn.Write(msg)
-	if err != nil {
-		fmt.Println("Failed to send to overlay socket: ", err)
-	}
-}
-
-// Exit overlay mode and hide overlay
-func exitOverlayMode() {
-	overlayMode = false
-	hideOverlay()
-}
-
-// Enter ovelay mode
-func enterOverlayMode() {
-	overlayMode = true
 }
 
 // Separate process to monitor held movement keys
@@ -388,6 +50,67 @@ func movementLoop() {
 		movementMutex.RUnlock()
 		time.Sleep(16 * time.Millisecond)
 	}
+}
+
+// Kill the overlay process
+func terminateOverLay() {
+	if overlayProc != nil && overlayProc.Process != nil {
+		_ = overlayProc.Process.Kill()
+	}
+	exec.Command("pkill", "-f", "overlay.py").Run()
+}
+
+// Display the overlay showing the division
+func showOverlay() {
+	messageOverlay([]byte(fmt.Sprintf("show, %v, %v, %v, %v, %v, %v, %v",
+		divCount, currentDivBoxX, currentDivBoxY,
+		divArea[divCount][0], divArea[divCount][1],
+		divDim[divCount][0], divDim[divCount][1])))
+}
+
+// Hide the overlay
+func hideOverlay() {
+	messageOverlay([]byte("hide"))
+}
+
+// Send message to overlay python process to change the overlay display
+func messageOverlay(msg []byte) {
+	conn, err := net.Dial("unix", socketPath)
+	if err != nil {
+		fmt.Println("Failed to connect to overlay socket: ", err)
+		return
+	}
+	defer conn.Close()
+	_, err = conn.Write(msg)
+	if err != nil {
+		fmt.Println("Failed to send to overlay socket: ", err)
+	}
+}
+
+// Move mouse to the center of the selected box during a division
+func mouseToBox() {
+
+	// Find index of pressed key in key map
+	var selectedKeyId int32 = divKeyMaps[divCount][pressed]	
+
+	// Calculate which box is selected
+	currentDivBoxX = currentDivBoxX + selectedKeyId % divDim[divCount][0] * divArea[divCount+1][0]
+	currentDivBoxY = currentDivBoxY + selectedKeyId / divDim[divCount][0] * divArea[divCount+1][1]
+
+	// Move mouse to the center of the box
+	var moveMouseX int32 = currentDivBoxX + divArea[divCount+1][0] / 2
+	var moveMouseY int32 = currentDivBoxY + divArea[divCount+1][1] / 2
+	mouseAbs(moveMouseX, moveMouseY)
+}
+
+// Reset division related variables
+func exitDivMode() {
+	hideOverlay()
+	divMode = false
+	divCount = -1
+	currentDivBoxX = 0
+	currentDivBoxY = 0
+	pressed = ""
 }
 
 // Main loop
@@ -434,7 +157,7 @@ func main() {
 				}
 
 				// Pass all captured key presses in normal mode
-				if (!mouseMode && !overlayMode) {
+				if (!mouseMode && !divMode) {
 					switch event.Value {
 					case 1:
 						keyboard.KeyDown(int(event.Code))
@@ -444,17 +167,6 @@ func main() {
 						// keyboard.KeyDown(int(event.Code))
 						continue
 					}
-				}
-
-				// Mousemode and user has pressed key to enter overlay
-				if (mouseMode && keyNames[event.Code] == overlayKey && event.Value == 0) {
-					mouseMode = false
-					enterOverlayMode()
-					overlay01 = false
-					overlay02 = false
-					overlay1 = false
-					showOverlay0()
-					continue
 				}
 
 				// Register holding and releasing movement keys
@@ -476,56 +188,87 @@ func main() {
 					}
 				}
 
-				// User has pressed key in overlay mode
-				if (overlayMode && event.Value == 0) {
-					// If current keystroke is the first input after overlay mode is activated
-					if (!overlay01) {
-						selectedDiv0Col = div0XKeyMap[keyNames[event.Code]]
-						overlay01 = true
+				// Mousemode and user has pressed key to enter overlay
+				if (mouseMode && keyNames[event.Code] == divKey && event.Value == 0) {
+					mouseMode = false
+					divMode = true // Must clear heldMovementKeys when entering divMode
+					divCount ++
+					showOverlay()
+					continue
+				}
 
-					// If current keystroke is the second input after overlay mode is activated
-					} else if (overlay01 && !overlay02) {
-
-						// Calculate coordinates
-						if (selectedDiv0Col < div0Cols / 2) {
-							selectedDiv0Row = div0Y0KeyMap[keyNames[event.Code]]
-						} else {
-							selectedDiv0Row = div0Y1KeyMap[keyNames[event.Code]]
+				if (divMode && event.Value == 0) {
+					pressed += keyNames[event.Code]
+					if _,ok := divKeyMaps[divCount][pressed]; ok {
+						fmt.Println("Pressed in map")
+						mouseToBox()
+						if (divCount == nDivs - 1) {
+							divMode = false
+							exitDivMode()
+						} else if (divCount < nDivs - 1) {
+							pressed = ""
+							showOverlay()
+							divCount ++
 						}
-						overlay02 = true
-
-						// Calculate the absolute position of the center of the selected box
-						var selectedBox0Xabs = selectedDiv0Col * box0X + box0X / 2
-						var selectedBox0Yabs = selectedDiv0Row * box0Y + box0Y / 2
-
-						// Move the mouse to the center of the selected box and display second overlay
-						mouseAbs(selectedBox0Xabs, selectedBox0Yabs)
-						showOverlay1(selectedDiv0Col, selectedDiv0Row)
-
-						if (adjustMode == 1) {
-							exitOverlayMode()
-							mouseMode = true
-						} else if (adjustMode == 2) {
-							continue
+					} else {
+						fmt.Println("Pressed not in map: ", pressed)
+						if (len(pressed) >= longestKeyLen[divCount]) {
+							divMode = false
+							exitDivMode()
 						}
-
-					// If current keystroke is the third input after overlay mode is activated
-					} else if (overlay01 && overlay02 && !overlay1) {
-						var selectedBox1 int32 = div1KeyMap[keyNames[event.Code]]
-
-						// Calculate the absolute position of the center of the selected box
-						var selectedBox1Xabs = selectedDiv0Col * box0X + selectedBox1 % div1Cols * box1X + box1X / 2
-						var selectedBox1Yabs = selectedDiv0Row * box0Y + selectedBox1 / div1Cols * box1Y + box1Y / 2
-
-						// Move the mouse to the center of the selected box and exit overlay
-						mouseAbs(selectedBox1Xabs, selectedBox1Yabs)
-						exitOverlayMode()
-
-						// Wait for overlay to exit
-						time.Sleep(10 * time.Millisecond)
-						mouse.LeftClick()
 					}
 				}
+
+				// // User has pressed key in overlay mode
+				// if (divMode && event.Value == 0) {
+				// 	// If current keystroke is the first input after overlay mode is activated
+				// 	if (!overlay01) {
+				// 		selectedDivCol = div0XKeyMap[keyNames[event.Code]]
+				// 		overlay01 = true
+
+				// 	// If current keystroke is the second input after overlay mode is activated
+				// 	} else if (overlay01 && !overlay02) {
+
+				// 		// Calculate coordinates
+				// 		if (selectedDivCol < div0Cols / 2) {
+				// 			selectedDivRow = div0Y0KeyMap[keyNames[event.Code]]
+				// 		} else {
+				// 			selectedDivRow = div0Y1KeyMap[keyNames[event.Code]]
+				// 		}
+				// 		overlay02 = true
+
+				// 		// Calculate the absolute position of the center of the selected box
+				// 		var selectedBox0Xabs = selectedDivCol * box0X + box0X / 2
+				// 		var selectedBox0Yabs = selectedDivRow * box0Y + box0Y / 2
+
+				// 		// Move the mouse to the center of the selected box and display second overlay
+				// 		mouseAbs(selectedBox0Xabs, selectedBox0Yabs)
+				// 		showOverlay1(selectedDivCol, selectedDivRow)
+
+				// 		if (adjustMode == 1) {
+				// 			exitDivMode()
+				// 			mouseMode = true
+				// 		} else if (adjustMode == 2) {
+				// 			continue
+				// 		}
+
+				// 	// If current keystroke is the third input after overlay mode is activated
+				// 	} else if (overlay01 && overlay02 && !overlay1) {
+				// 		var selectedBox1 int32 = div1KeyMap[keyNames[event.Code]]
+
+				// 		// Calculate the absolute position of the center of the selected box
+				// 		var selectedBox1Xabs = selectedDivCol * box0X + selectedBox1 % div1Cols * box1X + box1X / 2
+				// 		var selectedBox1Yabs = selectedDivRow * box0Y + selectedBox1 / div1Cols * box1Y + box1Y / 2
+
+				// 		// Move the mouse to the center of the selected box and exit overlay
+				// 		mouseAbs(selectedBox1Xabs, selectedBox1Yabs)
+				// 		exitDivMode()
+
+				// 		// Wait for overlay to exit
+				// 		time.Sleep(10 * time.Millisecond)
+				// 		mouse.LeftClick()
+				// 	}
+				// }
 			}
 		}
 	}
